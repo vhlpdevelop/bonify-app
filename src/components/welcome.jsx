0bfef62685aad5c65fdf93e2a0ca33940279626b
vitao <vhlp.develop@gmail.com>
2025-02-20 11:33:56 -0400

    netlify toml added

--- src/components/welcome.jsx
+++ src/components/welcome.jsx
@@ -3,22 +3,21 @@ import { Card, CardContent, Typography, Box } from '@mui/material';
 const welcome = () => {
     return (
         <Box
-        display="flex"
-        justifyContent="center"
-        alignItems="center"
-        minHeight="100vh"
-        sx={{ bgcolor: 'background.default', p: 3 }}
-    >
-        <Card>
-            <CardContent>
-             <Typography variant="h5" component="div">
-                                 BEM-VINDO
-             
-                                 </Typography>
-            </CardContent>
-        </Card>
-    </Box>
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+            minHeight="100vh"
+            sx={{ bgcolor: 'background.default', p: 3 }}
+        >
+            <Card>
+                <CardContent>
+                    <Typography variant="h5" component="div">
+                        BEM-VINDO
+                    </Typography>
+                </CardContent>
+            </Card>
+        </Box>
     );
-  };
-  
-  export default welcome;
+};
+
+export default welcome;
