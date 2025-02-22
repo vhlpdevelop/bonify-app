d0b25e19a117ceb7ad354cb84ab4007f61ef1b0e
Victor Hugo Lopes Poli <vhlp.develop@gmail.com>
2025-02-22 11:24:52 -0400

    teste 2

--- src/components/Advertisement.jsx
+++ src/components/Advertisement.jsx
@@ -4,8 +4,10 @@ import registryInteraction from '../services/registryInteraction';
 import LinearProgress from './LinearProgress';
 
 const Advertisement = ({ ads, params }) => {
+  console.log("ADS =>")
+  console.log(ads)
   const [showAdvertisement, setShowAdvertisement] = useState(false);
-  const [tempoRestante, setTempoRestante] = useState(ads.duration); // Inicia com 5 segundos
+  const [tempoRestante, setTempoRestante] = useState(5); // Inicia com 5 segundos
 
   useEffect(() => {
     if (tempoRestante > 0) {
